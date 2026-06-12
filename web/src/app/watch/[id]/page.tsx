22ce625a0aa37490218f5b591a6dda8e048000a1
Bayu Anta <bayuantadota@gmail.com>
2026-06-12 22:35:18 +0700

    Revert sandbox attribute due to player block

--- web/src/app/watch/[id]/page.tsx
+++ web/src/app/watch/[id]/page.tsx
@@ -35,7 +35,6 @@ export default async function WatchPage({ params }: { params: Promise<{ id: stri
               frameBorder="0" 
               scrolling="no"
               allow="autoplay; fullscreen; encrypted-media"
-              sandbox="allow-scripts allow-same-origin allow-presentation"
               allowFullScreen
               title={match.title}
             ></iframe>
